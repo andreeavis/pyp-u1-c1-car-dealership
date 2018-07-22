4156c811b178aa0a1c5857db04117606bd1db957
andreea.visanoiu <andreea.visanoiu@gmail.com>
2018-07-22 01:08:16 -0400

    Corrected super() in customers.py to pass Py2.7 test

--- dealership/customers.py
+++ dealership/customers.py
@@ -10,21 +10,10 @@ class Person(object):
 
 # defines sub-class (the parameters is the main Class)
 class Customer(Person):
-  # this is actually not needed -
-  # https://micropyramid.com/blog/understand-self-and-__init__-method-in-python-class/
-  # first_name, last_name, email are variables of the class 
-  def __init__(self, first_name, last_name, email):
-    # super() calls the information from the Class 
-    super().__init__(first_name, last_name, email)
-    
-  # defines the function is_employee as false (as the Customer is not employee)
   def is_employee(self):
-    return False 
-  
-
+    return False
+ 
+   
 class Employee(Person):
-    def __init__(self, first_name, last_name, email):
-      super().__init__(first_name, last_name, email)
-      
-    def is_employee(self):
-      return True
+  def is_employee(self):
+    return True
